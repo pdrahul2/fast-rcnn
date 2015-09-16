b117107cb13f6ada4fad2b2e08e264f67e91fef9
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-09-16 13:41:26 -0700

    scripts for testing pre-trained models and training and testing models on NYUD2

--- experiments/test_pretrained_models.sh
+++ experiments/test_pretrained_models.sh
@@ -3,8 +3,7 @@ tr_set='trainval'
 test_set='test'
 
 modality="images+hha"
-# for model in "alexnet_rgb_alexnet_hha" "vgg_rgb_alexnet_hha"; do
-for model in "vgg_rgb_alexnet_hha"; do
+for model in "alexnet_rgb_alexnet_hha" "vgg_rgb_alexnet_hha"; do
   PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
     --def output/$model/test.prototxt.$modality \
     --net output/$model/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
--- experiments/train_models.sh
+++ experiments/train_models.sh
@@ -5,8 +5,7 @@ PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 0 \
   --imdb nyud2_hha_2015_trainval \
   --cfg output/training_demo_hha/config.prototxt.hha \
   --iters 40000 \
-  2>&1 | tee output/training_demo_hha/train.log
-
+  2>&1 | tee output/training_demo_hha/train.log.hha
 
 # Train the VGG model on the color images
 PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 2 \
@@ -14,7 +13,7 @@ PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 2 \
   --weights data/init_models/VGG16/VGG16.v2.caffemodel \
   --imdb nyud2_images_2015_trainval \
   --cfg output/training_demo_rgb_vgg/config.prototxt.images \
-  2>&1 | tee output/training_demo_rgb_vgg/train.log
+  2>&1 | tee output/training_demo_rgb_vgg/train.log.images
 
 # Train the alexnet model on the color images
 PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 0 \
@@ -22,6 +21,58 @@ PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 0 \
   --weights data/init_models/CaffeNet/CaffeNet.v2.caffemodel \
   --imdb nyud2_images_2015_trainval \
   --cfg output/training_demo_rgb_alexnet/config.prototxt.images \
-  2>&1 | tee output/training_demo_rgb_alexnet/train.log
+  2>&1 | tee output/training_demo_rgb_alexnet/train.log.images
 
+# Merge alexnet HHA and alexnet RGB models using by net surgery
+mkdir output/training_demo/alexnet_rgb_alexnet_hha/nyud2_images+hha_2015_trainval
+PYTHONPATH='.' python python_utils/do_net_surgery.py \
+  --out_net_def output/training_demo/alexnet_rgb_alexnet_hha/test.prototxt.images+hha \
+  --net_surgery_json output/training_demo/alexnet_rgb_alexnet_hha/init_weights.json \
+  --out_net_file output/training_demo/alexnet_rgb_alexnet_hha/nyud2_images+hha_2015_trainval/fast_rcnn_iter_40000.caffemodel
 
+# Merge alexnet HHA and VGG RGB models using by net surgery
+mkdir output/training_demo/vgg_rgb_alexnet_hha/nyud2_images+hha_2015_trainval
+PYTHONPATH='.' python python_utils/do_net_surgery.py \
+  --out_net_def output/training_demo/vgg_rgb_alexnet_hha/test.prototxt.images+hha \
+  --net_surgery_json output/training_demo/vgg_rgb_alexnet_hha/init_weights.json \
+  --out_net_file output/training_demo/vgg_rgb_alexnet_hha/nyud2_images+hha_2015_trainval/fast_rcnn_iter_40000.caffemodel
+
+# Testing alexnet_rgb and alexnet_hha models
+model='alexnet_rgb_alexnet_hha'; tr_set='trainval'; test_set='test'; modality="images+hha";
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+  --def output/training_demo/$model/test.prototxt.$modality \
+  --net output/training_demo/$model/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --cfg output/training_demo/$model/config.prototxt."$modality"
+
+# Testing vgg_rgb and alexnet_hha models
+model='vgg_rgb_alexnet_hha'; tr_set='trainval'; test_set='test'; modality="images+hha";
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+  --def output/training_demo/$model/test.prototxt.$modality \
+  --net output/training_demo/$model/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --cfg output/training_demo/$model/config.prototxt."$modality"
+
+# Testing alexnet_hha models
+model='alexnet_hha'; tr_set='trainval'; test_set='test'; modality="hha";
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+  --def output/training_demo/$model/test.prototxt.$modality \
+  --net output/training_demo/$model/nyud2_"$modality"_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --cfg output/training_demo/$model/config.prototxt."$modality"
+
+# Testing alexnet_rgb models
+model='alexnet_rgb'; tr_set='trainval'; test_set='test'; modality="images";
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+  --def output/training_demo/$model/test.prototxt.$modality \
+  --net output/training_demo/$model/nyud2_"$modality"_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --cfg output/training_demo/$model/config.prototxt."$modality"
+
+# Testing vgg_rgb models
+model='vgg_rgb'; tr_set='trainval'; test_set='test'; modality="images";
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+  --def output/training_demo/$model/test.prototxt.$modality \
+  --net output/training_demo/$model/nyud2_"$modality"_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --cfg output/training_demo/$model/config.prototxt."$modality"
