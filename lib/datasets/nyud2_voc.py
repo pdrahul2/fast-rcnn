a28550b1ee0e9e0f2c56bb1b38cc55ce90ad124f
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-10-07 15:28:53 -0700

    Also reads in instance id

--- lib/datasets/nyud2_voc.py
+++ lib/datasets/nyud2_voc.py
@@ -171,25 +171,29 @@ class nyud2_voc(datasets.imdb):
         
         num_objs = len(objs)
 
-        boxes = np.zeros((num_objs, 4), dtype=np.uint16)
-        gt_classes = np.zeros((num_objs), dtype=np.int32)
-        overlaps = np.zeros((num_objs, self.num_classes), dtype=np.float32)
+        boxes       = np.zeros((num_objs, 4), dtype=np.uint16)
+        instance_id = np.zeros((num_objs), dtype=np.uint16)
+        gt_classes  = np.zeros((num_objs), dtype=np.int32)
+        overlaps    = np.zeros((num_objs, self.num_classes), dtype=np.float32)
 
         # Load object bounding boxes into a data frame.
         for ix, obj in enumerate(objs):
             # Make pixel indexes 0-based
-            cls = self._class_to_ind.get(str(obj['class'][0]))
-            boxes[ix, :] = obj['bbox'][0] - 1
-            gt_classes[ix] = cls
+            cls               = self._class_to_ind.get(str(obj['class'][0]))
+            boxes[ix, :]      = obj['bbox'][0] - 1
+            gt_classes[ix]    = cls
+            instance_id[ix]   = obj['instanceId'][0]
             overlaps[ix, cls] = 1.0
-
-        overlaps = scipy.sparse.csr_matrix(overlaps)
-
-        return {'boxes' : boxes,
-                'gt_classes': gt_classes,
+        
+        if num_objs > 0:
+          overlaps = scipy.sparse.csr_matrix(overlaps)
+        
+        return {'boxes'       : boxes,
+                'gt_classes'  : gt_classes,
                 'gt_overlaps' : overlaps,
-                'flipped' : False}
-    
+                'flipped'     : False,
+                'instance_id' : instance_id}
+
     def evaluate_detections(self, all_boxes, output_dir, det_salt = '', eval_salt = '', overlap_thresh = 0.5):
       num_classes = self.num_classes
       num_images = self.num_images
