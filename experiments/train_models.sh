3e32f5d7921a373cebd12b44d82aa531316033c2
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2016-01-27 16:40:47 -0800

    fixes to train_model script to be compatible with new dir names

--- experiments/train_models.sh
+++ experiments/train_models.sh
@@ -1,27 +1,27 @@
 # Train the HHA Alexnet model from the supervision transfer initialization weights
 PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 0 \
-  --solver output/training_demo_hha/solver.prototxt.hha \
+  --solver output/training_demo/alexnet_hha/solver.prototxt.hha \
   --weights data/init_models/ST_vgg_to_alexnet_hha/ST_vgg_to_alexnet_hha.caffemodel \
   --imdb nyud2_hha_2015_trainval \
-  --cfg output/training_demo_hha/config.prototxt.hha \
+  --cfg output/training_demo/alexnet_hha/config.prototxt.hha \
   --iters 40000 \
-  2>&1 | tee output/training_demo_hha/train.log.hha
+  2>&1 | tee output/training_demo/alexnet_hha/train.log.hha
 
 # Train the VGG model on the color images
 PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 2 \
-  --solver output/training_demo_rgb_vgg/solver.prototxt.images \
+  --solver output/training_demo/vgg_rgb/solver.prototxt.images \
   --weights data/init_models/VGG16/VGG16.v2.caffemodel \
   --imdb nyud2_images_2015_trainval \
-  --cfg output/training_demo_rgb_vgg/config.prototxt.images \
-  2>&1 | tee output/training_demo_rgb_vgg/train.log.images
+  --cfg output/training_demo/vgg_rgb/config.prototxt.images \
+  2>&1 | tee output/training_demo/vgg_rgb/train.log.images
 
 # Train the alexnet model on the color images
 PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/train_net.py --gpu 0 \
-  --solver output/training_demo_rgb_alexnet/solver.prototxt.images \
+  --solver output/training_demo/alexnet_rgb/solver.prototxt.images \
   --weights data/init_models/CaffeNet/CaffeNet.v2.caffemodel \
   --imdb nyud2_images_2015_trainval \
-  --cfg output/training_demo_rgb_alexnet/config.prototxt.images \
-  2>&1 | tee output/training_demo_rgb_alexnet/train.log.images
+  --cfg output/training_demo/alexnet_rgb/config.prototxt.images \
+  2>&1 | tee output/training_demo/alexnet_rgb/train.log.images
 
 # Merge alexnet HHA and alexnet RGB models using by net surgery
 mkdir output/training_demo/alexnet_rgb_alexnet_hha/nyud2_images+hha_2015_trainval
