c8a4b1226a85688f5944764159792a07cbdaad11
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2016-03-06 15:19:22 -0800

    paths..

--- _init_paths.py
+++ _init_paths.py
@@ -17,15 +17,17 @@ def add_path(path):
 
 this_dir = osp.dirname(__file__)
 
-# Add caffe to PYTHONPATH
-# caffe_path = osp.join('caffe', 'python', 'caffe')
-# add_path(caffe_path)
+# Use this when you want to compute object detection output, for example when
+# using scripts experiments/test_pretrained_models.sh experiments/train_models.sh
+if False:
+  caffe_path = osp.join(this_dir, 'caffe-fast-rcnn', 'python')
+  add_path(caffe_path)
 
-caffe_path = osp.join(this_dir, 'caffe-fast-rcnn', 'python')
-add_path(caffe_path)
-
-# caffe_path = osp.join(this_dir, 'caffe', 'build_' + platform.node(), 'python')
-# add_path(caffe_path)
+# Use this when you are computing SDS output, for example when using scripts
+# experiments/sds_test_pretrained_models.sh experiments/sds_train_models.sh  
+if True:
+  caffe_path = osp.join(this_dir, 'caffe-sds', 'python')
+  add_path(caffe_path)
 
 # Add lib to PYTHONPATH
 lib_path = osp.join(this_dir, 'lib')
