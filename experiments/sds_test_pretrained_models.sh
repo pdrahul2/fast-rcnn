60e77d82ab0b386b90adb0c71604cf44252b4979
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2016-03-05 19:44:15 -0800

    script to test the pre-trained models

---
+++ experiments/sds_test_pretrained_models.sh
@@ -0,0 +1,38 @@
+set -x
+
+# Testing a RGB + HHA model
+modality="images+hha"
+hha_model="alexnet_hha"
+for rgb_model in "alexnet_rgb" "vgg_rgb"; do
+  PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 0 \
+    --def output/sds/$rgb_model"_"$hha_model/test.prototxt.$modality \
+    --net output/sds/$rgb_model/nyud2_images_2015_trainval/inst_segm_iter_10000.caffemodel output/sds/$hha_model/nyud2_hha_2015_trainval/inst_segm_iter_10000.caffemodel \
+    --imdb nyud2_$modality"_2015_test" \
+    --sds 1 --sds_sp_dir data/nyud2_sp --sds_detection_file output/$rgb_model"_"$hha_model/nyud2_images+hha_2015_test/fast_rcnn_iter_40000/detections.pkl \
+    --sds_img_blob_name image da_image --sds_output_blob_name fusion_loss --sds_sp_thresh 0.4 --sds_save_output 1 \
+    --set EXP_DIR sds/$rgb_model"_"$hha_model SDS.TARGET_SIZE 688 
+done
+
+# Testing RGB only models
+modality="images"
+for model in "alexnet_rgb" "vgg_rgb"; do
+  PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+    --def output/sds/$model/test.prototxt.$modality \
+    --net output/sds/$model/nyud2_$modality""_2015_trainval/inst_segm_iter_10000.caffemodel \
+    --imdb nyud2_$modality"_2015_test" \
+    --sds 1 --sds_sp_dir data/nyud2_sp --sds_detection_file output/$model/nyud2_$modality"_2015_test"/fast_rcnn_iter_40000/detections.pkl \
+    --sds_img_blob_name image --sds_output_blob_name loss --sds_sp_thresh 0.4 \
+    --set EXP_DIR sds/$model SDS.TARGET_SIZE 688
+done
+
+# Testing HHA only model
+modality="hha"
+for model in "alexnet_hha"; do
+  PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+    --def output/sds/$model/test.prototxt.$modality \
+    --net output/sds/$model/nyud2_$modality""_2015_trainval/inst_segm_iter_10000.caffemodel \
+    --imdb nyud2_$modality"_2015_test" \
+    --sds 1 --sds_sp_dir data/nyud2_sp --sds_detection_file output/$model/nyud2_$modality"_2015_test"/fast_rcnn_iter_40000/detections.pkl \
+    --sds_img_blob_name image --sds_output_blob_name loss --sds_sp_thresh 0.4 \
+    --set EXP_DIR sds/$model SDS.TARGET_SIZE 688
+done
