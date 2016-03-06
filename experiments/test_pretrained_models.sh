b951991ea01710df717666452f31c0f739616d37
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2016-03-06 00:49:46 -0800

    testing pre-trained rgb only and hha only models

--- experiments/test_pretrained_models.sh
+++ experiments/test_pretrained_models.sh
@@ -10,3 +10,39 @@ for model in "alexnet_rgb_alexnet_hha" "vgg_rgb_alexnet_hha"; do
     --imdb nyud2_"$modality"_2015_"$test_set" \
     --cfg output/$model/config.prototxt
 done
+
+# test only alexnet RGB 
+model="alexnet_rgb"
+modality="images"
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+  --def output/$model/test.prototxt.$modality \
+  --net output/alexnet_rgb_alexnet_hha/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --set EXP_DIR $model TEST.SCALES [688] 
+
+# test only alexnet HHA 
+model="alexnet_hha"
+modality="hha"
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+  --def output/$model/test.prototxt.$modality \
+  --net output/alexnet_rgb_alexnet_hha/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --set EXP_DIR $model TEST.SCALES [688] 
+
+
+# test only vgg RGB
+# vgg RGB model was trained with an image size of 600 and not 688. Testing this model
+# with image size of 600 gives 38.8, with image size of 688 gives 36.0. 
+model="vgg_rgb"
+modality="images"
+PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+  --def output/$model/test.prototxt.$modality \
+  --net output/vgg_rgb_alexnet_hha/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+  --imdb nyud2_"$modality"_2015_"$test_set" \
+  --set EXP_DIR $model TEST.SCALES [600]
+
+# PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
+#   --def output/$model/test.prototxt.$modality \
+#   --net output/vgg_rgb_alexnet_hha/nyud2_images+hha_2015_$tr_set/fast_rcnn_iter_40000.caffemodel \
+#   --imdb nyud2_"$modality"_2015_"$test_set" \
+#   --set EXP_DIR $model TEST.SCALES [688] TEST.DET_SALT _688
